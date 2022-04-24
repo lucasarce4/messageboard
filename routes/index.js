5efa48832eafa282912bb3f79159dae6f5dda415
Lucas Arce <lucasearce4@gmail.com>
2022-04-24 04:27:01 -0300

    1.0.0

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+const moment = require('moment')
+
+const messages = [
+  {
+    text: "My matress is too hard",
+    user: "Rosy",
+    added: moment().startOf('day').fromNow(),
+  },
+  {
+    text: "I like furry animals",
+    user: "Carlos",
+    added: moment().startOf('hour').fromNow(),
+  },
+  {
+    text: "What am I supposed to do now?",
+    user: "Ricardo",
+    added: moment().startOf('week').fromNow(),
+  },
+];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Leave a message!',messages: messages  });
 });
+
+router.post('/new',(req,res,next)=>{
+  messages.unshift({
+    text:req.body.message,
+    user:req.body.name,
+    added: moment().startOf('hour' - 1).fromNow(),
+  })
+  res.redirect('/')
+})
+
 
 module.exports = router;
